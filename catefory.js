d0efc5fc2828d48d22b6c446c162266871425912
ErrayAbdellah <mahdiabdllah121@gmail.com>
2023-04-28 23:17:23 +0100

    update uml

---
+++ catefory.js
@@ -0,0 +1,32 @@
+var categoryObject = [
+                        {"value":"","label":"Type de propri\u00e9t\u00e9"},
+                        {"value":1,"request_value":1,"label":"Appartement"},
+                        {"value":35,"request_value":35,"label":"Villa"},
+                        {"value":22,"request_value":22,"label":"Maison"},
+                        {"value":24,"request_value":24,"label":"Duplex"},
+                        {"value":10,"request_value":10,"label":"Immeuble"},
+                        {"value":5,"request_value":5,"label":"Terrain"},
+                        {"value":31,"request_value":31,"label":"Bungalow"},
+                        {"value":44,"request_value":44,"label":"Chalet"},
+                        {"value":45,"request_value":45,"label":"Usine"},
+                        {"value":48,"request_value":48,"label":"Riad"},
+                        {"value":56,"request_value":56,"label":"Triplex"},
+                        {"value":58,"request_value":58,"label":"Studio"},
+                        {"value":63,"request_value":63,"label":"Penthouse"},
+                        {"value":69,"request_value":69,"label":"Hangar"}
+                    ]
+
+
+            // Choisir une catégorie
+            // Appartement
+            // Villa
+            // Studio
+            // Maison
+            // Terrain agricol
+            // Terrain nu
+            // Bureau
+            // Local commercial
+            // Local industriel
+            // Entrepôt et dépôt
+            // Riad
+            // Ferme
